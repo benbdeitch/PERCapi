39a0b8734e46b348f16cac1c25ecf258c84f47fc
benbdeitch <benbdeitch@gmail.com>
2024-02-26 17:12:40 -0500

    Added data routes to imports.

--- app/blueprints/API/__init__.py
+++ app/blueprints/API/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-from . import auth_routes, helper_functions
+from . import auth_routes, helper_functions, data_routes
