d7491ba45b6dbbc3313b2009f7fce5665b35b362
benbdeitch <benbdeitch@gmail.com>
2024-03-15 17:25:28 -0400

    Added pending routes to imports.

--- app/blueprints/API/__init__.py
+++ app/blueprints/API/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-from . import auth_routes, helper_functions, data_routes
+from . import auth_routes, helper_functions, pending_routes
